b3be3357608c84316826e08e6bd9f27e8a3a5aaa
yanshen <yanshen@qiniu.com>
2019-03-19 20:16:18 +0800

    添加基础功能，监听俩端口，admin和业务

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"time"
 )
+
+type defaultconfig struct {
+	address string
+	port    string
+}
+
+var dc = defaultconfig{
+	address: "127.0.0.1",
+	port:    "8000",
+}
 
 func main() {
 	fmt.Println("Start a proxy instance ...")
+	go Listen(dc)
+	admHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "admin page\n")
+	}
+	http.HandleFunc("/admin", admHandler)
+	http.ListenAndServe(":9001", nil)
+}
+
+func Listen(dc defaultconfig) {
+	lis, err := net.Listen("tcp", dc.address+":"+dc.port)
+	if err != nil {
+		fmt.Println("listener error ", err)
+	}
+	go handleconn(lis)
+	time.Sleep(time.Second * 20)
+}
+
+func handleconn(l net.Listener) {
+	fmt.Println(l.Addr().Network())
 }
